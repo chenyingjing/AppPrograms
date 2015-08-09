889b495d9f018e73b4d931436d77e3f2daef6596
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-09 11:57:33 +0800

    It is about how to se App icon and launch image.

---
+++ appIcon/appIcon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appIcon
+//
+//  Created by aa64mac on 8/9/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ appIcon/appIcon/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  appIcon
+//
+//  Created by aa64mac on 8/9/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
