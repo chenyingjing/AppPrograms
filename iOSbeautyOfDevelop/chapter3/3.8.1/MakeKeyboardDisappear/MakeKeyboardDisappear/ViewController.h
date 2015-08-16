d88eb3b85ccc0504ae37fc998695a705c3a7f88f
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-16 12:04:42 +0800

    Make keyboard disappear by delegate.

---
+++ iOSbeautyOfDevelop/chapter3/3.8.1/MakeKeyboardDisappear/MakeKeyboardDisappear/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MakeKeyboardDisappear
+//
+//  Created by aa64mac on 8/16/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController<UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textField1;
+
+@end
+
