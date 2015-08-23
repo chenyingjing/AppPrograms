96cfa6ef1ef17e454e08171c7e30497c5bc51709
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-23 10:40:04 +0800

    Both the action and event will be triggered.

---
+++ iOSbeautyOfDevelop/chapter5/5.2/segue/segue/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  segue
+//
+//  Created by aa64mac on 8/23/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonTouchUpInside:(id)sender {
+    int x = 0;
+    x++;
+}
+
+@end
