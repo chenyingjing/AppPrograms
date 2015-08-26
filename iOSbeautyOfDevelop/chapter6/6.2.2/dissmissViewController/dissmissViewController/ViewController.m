90e93f2ffd6448ac191cfe79d1649b96c2a81e63
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-26 20:35:25 +0800

    dismiss viewcontrol

---
+++ iOSbeautyOfDevelop/chapter6/6.2.2/dissmissViewController/dissmissViewController/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  dissmissViewController
+//
+//  Created by aa64mac on 8/26/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonTouchUpInside:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
