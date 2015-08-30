8d6ffdc8ce46bbf8753312a640475f89774157a7
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-30 12:21:26 +0800

    Table view1 in swift

---
+++ iOSbeautyOfDevelop/chapter8/MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let defaultColorName = "red"
+var userDefinedColorName: String?   // defaults to nil
+
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+
+var a: Int?
+
+var x = a ?? 10
+
+
