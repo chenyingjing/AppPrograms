192d3b51ef6d592e44c233f98c7f146d6ca5c82b
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-30 16:37:00 +0800

    Get instance by Tag.

---
+++ iOSbeautyOfDevelop/chapter9/9.2/TableView1/TableView1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView1
+//
+//  Created by aa64mac on 8/30/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSbeautyOfDevelop/chapter9/9.2/TableView1/TableView1/RadioTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  RadioTableViewController.h
+//  TableView1
+//
+//  Created by aa64mac on 8/30/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadioTableViewController : UITableViewController
+
+@property NSArray *radioListArray;
+@property NSArray *radioEnglishArray;
+@property NSArray *radioImages;
+
+@end
