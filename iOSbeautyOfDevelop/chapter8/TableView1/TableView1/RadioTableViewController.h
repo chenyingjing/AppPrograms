f81f0b3636c41b3e15bde6f1b7b7310b1d00ce63
aa64mac <aa64mac@aa64macs-iMac.local>
2015-08-30 11:20:51 +0800

    table view 1.

---
+++ iOSbeautyOfDevelop/chapter8/TableView1/TableView1/RadioTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  RadioTableViewController.h
+//  TableView1
+//
+//  Created by aa64mac on 8/30/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadioTableViewController : UITableViewController
+
+@property NSArray *radioListArray;
+
+@end
