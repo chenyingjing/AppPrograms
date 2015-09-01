ff9d9cff9f3fd7beeb79a0ca2e2490de5d48d2ef
aa64mac <aa64mac@aa64macs-iMac.local>
2015-09-01 20:33:43 +0800

    Load data from plist.

---
+++ iOSbeautyOfDevelop/chapter11/TableView1/TableView1/RadioTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RadioTableViewCell.m
+//  TableView1
+//
+//  Created by aa64mac on 8/30/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import "RadioTableViewCell.h"
+
+@implementation RadioTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
