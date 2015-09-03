e15141913e9a28f1e348466d0fa59e504eda3592
aa64mac <aa64mac@aa64macs-iMac.local>
2015-09-03 16:51:58 +0800

    refresh

---
+++ iOSbeautyOfDevelop/chapter11/11.8/TableView1/TableView1/RadioTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  RadioTableViewCell.h
+//  TableView1
+//
+//  Created by aa64mac on 8/30/15.
+//  Copyright (c) 2015 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadioTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *mainTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subTitleLabel;
+
+
+@end
