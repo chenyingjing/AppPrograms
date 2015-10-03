52cf2d48a3eb0b23b457b042fd0fececc5339f47
aa64mac <aa64mac@aa64macs-imac.local>
2015-10-03 11:56:06 +0800

    iOS Getting Started with Swift: FoodTracker

---
+++ Start Developing iOS Apps (Swift)/FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by aa64mac on 10/2/15.
+//
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
