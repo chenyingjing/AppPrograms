45016df41171f729ebf830a3c576fd00bde99d51
aa64mac <aa64mac@aa64macs-imac.local>
2015-11-09 21:33:01 +0800

    Create D86QuartzTranslateScaleRotate project.

---
+++ Quartz/D86QuartzTranslateScaleRotate/D86QuartzTranslateScaleRotate/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  D86QuartzTranslateScaleRotate
+//
+//  Created by aa64mac on 11/9/15.
+//  Copyright © 2015 cyj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func leftButtonTouchUpInside(sender: UIButton) {
+    }
+
+    @IBAction func upButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    @IBAction func downButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    @IBAction func rightButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    @IBAction func clockWiseButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    @IBAction func anticlockButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    @IBAction func zoominButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    @IBAction func zoomoutButtonTouchUpInside(sender: UIButton) {
+    }
+    
+    
+    
+}
+
