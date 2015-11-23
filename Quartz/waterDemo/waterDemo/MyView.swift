ef5aaf6d5732a77fa9c38e3ee4e41503c9256e15
aa64mac <aa64mac@aa64macs-imac.local>
2015-11-23 20:23:02 +0800

    Start to do water demo.

---
+++ Quartz/waterDemo/waterDemo/MyView.swift
@@ -0,0 +1,39 @@
+//
+//  MyView.swift
+//  waterDemo
+//
+//  Created by aa64mac on 11/23/15.
+//  Copyright © 2015 cyj. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    override func drawRect(rect: CGRect) {
+        drawLine()
+    }
+    
+    func drawLine() {
+        let context: CGContextRef! = UIGraphicsGetCurrentContext()
+        CGContextSaveGState(context)
+        
+        CGContextSetLineWidth(context, 20)
+        CGContextSetLineCap(context, CGLineCap.Round)
+        
+        CGContextSetRGBStrokeColor(context, 1, 0, 0, 1)
+        CGContextMoveToPoint(context, 10, 10)
+        CGContextAddLineToPoint(context, 100, 100)
+        CGContextStrokePath(context)
+        CGContextRestoreGState(context)
+        
+        UIColor.blueColor().set()
+        
+        CGContextSetLineJoin(context, CGLineJoin.Round)
+        CGContextMoveToPoint(context, 100, 120)
+        CGContextAddLineToPoint(context, 150, 120)
+        CGContextAddLineToPoint(context, 150, 180)
+        CGContextStrokePath(context)
+    }
+
+}
---
+++ Quartz/waterDemo/waterDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  waterDemo
+//
+//  Created by aa64mac on 11/23/15.
+//  Copyright © 2015 cyj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func leftAction(sender: UIButton) {
+    }
+
+    @IBAction func upAction(sender: UIButton) {
+    }
+    
+    @IBAction func downAction(sender: UIButton) {
+    }
+    
+    @IBAction func rightAction(sender: UIButton) {
+    }
+    
+}
+
